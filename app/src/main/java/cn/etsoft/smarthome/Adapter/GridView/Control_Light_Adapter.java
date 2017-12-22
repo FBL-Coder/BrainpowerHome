3d7985b611ad156cfacb02168d99cdb1cf588f09
fubolong <18153986035@163.com>
2017-12-22 12:39:38 +0800

    添加灯光调节亮度功能

--- app/src/main/java/cn/etsoft/smarthome/Adapter/GridView/Control_Light_Adapter.java
+++ app/src/main/java/cn/etsoft/smarthome/Adapter/GridView/Control_Light_Adapter.java
@@ -6,12 +6,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
+
+import com.jaygoo.widget.RangeSeekBar;
 
 import java.util.List;
 
+import cn.etsoft.smarthome.Domain.UdpProPkt;
 import cn.etsoft.smarthome.Domain.WareLight;
+import cn.etsoft.smarthome.MyApplication;
 import cn.etsoft.smarthome.R;
+import cn.etsoft.smarthome.Utils.SendDataUtil;
 
 /**
  * Author：FBL  Time： 2017/6/26.
@@ -57,22 +63,62 @@ public class Control_Light_Adapter extends BaseAdapter {
             convertView = LayoutInflater.from(mContext).inflate(R.layout.gridview_control_light_item, null);
             viewHoler.mName = (TextView) convertView.findViewById(R.id.Control_GridView_Item_Name);
             viewHoler.mIV = (ImageView) convertView.findViewById(R.id.Control_GridView_Item_IV);
+            viewHoler.seekBar = (RangeSeekBar) convertView.findViewById(R.id.Control_GridView_Item_Slide);
             convertView.setTag(viewHoler);
         } else viewHoler = (ViewHoler) convertView.getTag();
 
         if (mLights.size() == 0 || position > mLights.size() - 1)
             return convertView;
 
+        if (mLights.get(position).getbTuneEn() == 1) {
+            viewHoler.seekBar.setVisibility(View.VISIBLE);
+            viewHoler.seekBar.setValue(mLights.get(position).getLmVal());
+        }
+
+
         if (mLights.get(position).getbOnOff() == 0)
             viewHoler.mIV.setImageResource(R.drawable.light_close);
         else viewHoler.mIV.setImageResource(R.drawable.light_open);
 
         viewHoler.mName.setText(mLights.get(position).getDev().getDevName());
+
+
+        viewHoler.mIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyApplication.mApplication.getSp().play(MyApplication.mApplication.getMusic(), 1, 1, 0, 0, 1);
+                if (mLights.get(position).getbOnOff() == 0)
+                    SendDataUtil.controlDev(mLights.get(position).getDev(), 0);
+                else SendDataUtil.controlDev(mLights.get(position).getDev(), 1);
+            }
+        });
+        viewHoler.seekBar.setOnRangeChangedListener(new RangeSeekBar.OnRangeChangedListener() {
+            int min = 0;
+
+            @Override
+            public void onRangeChanged(RangeSeekBar rangeSeekBar, float v, float v1, boolean b) {
+                min = (int) v;
+            }
+
+            @Override
+            public void onStartTrackingTouch(RangeSeekBar rangeSeekBar, boolean b) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(RangeSeekBar rangeSeekBar, boolean b) {
+                if (min < mLights.get(position).getLmVal())
+                    SendDataUtil.controlLight(mLights.get(position), UdpProPkt.E_LGT_CMD.e_lgt_dark.getValue(), min);
+                else if (min > mLights.get(position).getLmVal())
+                    SendDataUtil.controlLight(mLights.get(position), UdpProPkt.E_LGT_CMD.e_lgt_bright.getValue(), min);
+            }
+        });
         return convertView;
     }
 
     class ViewHoler {
         ImageView mIV;
         TextView mName;
+        RangeSeekBar seekBar;
     }
 }
